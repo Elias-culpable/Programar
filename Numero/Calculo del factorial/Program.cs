9d3cae7560847e314eda09ebbceb417004724cb9
Christian Elias <elias5684@gmail.com>
2023-01-19 13:47:10 +0100

    Numeros

---
+++ Numero/Calculo del factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace calculo_del_factorial
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Calculo del factorial");
+            Console.WriteLine("--------------------------");
+            Console.Write("Numero Factorial: ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            int Resul =1;
+            if (Num<0)
+            {
+                Console.Write($"No se pueden");
+            }
+            else
+            {
+                for (int i = 1; i <= Num; i++)
+                {
+                    Resul = Resul * i;
+                }
+            }
+            
+            Console.Write($"El resultado del factorial de {Num} es: {Resul}");
+        }
+    }
+}
---
+++ Numero/Segundos a (H-M-S)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace segundos_a_HMS
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Segundos a horas minutos y segundos");
+            Console.WriteLine("--------------------------");
+            Console.Write("Segundos: ");
+            int Num,H,M,S;
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            H =Num/3600;
+            M =(Num-3600*H)/60;
+            S = Num - (H * 3600 + M * 60);
+
+            Console.WriteLine($"El total en segundos de {Num} son {H} horas, {M} Minutos y {S} Segundos");
+        }
+    }
+}
